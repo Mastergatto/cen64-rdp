a0a097898e34d78fb77c9c41d996e939b6a93ad9
Tyler Stachecki <tstache1@cs.binghamton.edu>
2013-06-08 05:56:09 -0400

    Initial commit.
    Most egregious errors fixed: removed ^M at line ends, replaced
    [U]INT[0-9]* with standard types, expanded tabs, etc.

---
+++ Core.h
@@ -0,0 +1,15 @@
+/* ============================================================================
+ *  Core.h: Core RDP logic.
+ *
+ *  Original author: `angrylion`. Many thanks: to Ville Linde, `MooglyGuy`,
+ *  Shoutouts to: `olivieryuyu`, `marshallh`, `LaC`, `oman`, `pinchy`, `ziggy`,
+ *  `FatCat` and other folks I forgot.
+ *
+ *  This file is subject to the terms and conditions defined in
+ *  file 'MAMELICENSE', which is part of this source code package.
+ * ========================================================================= */
+#ifndef __RDP__CORE__
+#define __RDP__CORE__
+
+#endif
+
